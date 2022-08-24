806a455464a775c0dcbda9b6f26127d337b46f7a
skyy-yy <natthanichasi63@nu.ac.th>
2022-08-24 23:55:22 +0700

    abc

---
+++ src/app.js
@@ -0,0 +1,12 @@
+let express = require('express')
+const app = express()
+
+app.get('/status', function (req, res ){
+    res.send('Hello nodejs server')
+})
+
+let port = 8081
+
+app.listen(port, function() {
+    console.log('server running on ' + port)
+})
